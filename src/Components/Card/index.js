04d0dba92d274e33abf5f1c657e6a7b626e7c5db
andreicruz <andrei_cruz97@hotmail.com>
2019-10-20 03:56:08 -0300

    new function

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './style.sass';
 import luke from '../../assets/luke.jpg';
 import leia from '../../assets/leia.jpg';
-import darth from '../../assets/darth-vader.jpg';
+import r2d2 from '../../assets/darth-vader.jpg';
 import han from '../../assets/han-solo.jpg';
 import classNames from 'classnames'
 import ky from 'ky';
@@ -11,6 +11,11 @@ import ky from 'ky';
 export default function CardComponent(route) {
   const getPersons = () => ky.get(`https://swapi.co/api/${route.name}`).json();
   const [apiReturn, setReturn] = useState([]);
+  const [imagesCharacters] = useState([
+    // { name: 'luke', path: luke},
+    { name: 'leia', path: leia},
+    { name: 'r2', path: r2d2}
+  ]);
   const data = apiReturn.map(item => item.results)
 
   async function loadReturn() {
@@ -22,6 +27,20 @@ export default function CardComponent(route) {
     loadReturn();
   }, []);
   
+
+  // Todo: Ajustar para uma função
+  // Verifica se o objeto da api está no array de imagens
+  const datinha = [];
+  data.forEach(element => {
+    element.forEach(teste => {
+      imagesCharacters.forEach(character => {
+        if (character.name === teste.name.split((/[ -]/))[0].toLowerCase()) {
+          datinha.push(character)
+        }
+      })
+    })
+  })
+  console.log('newww ', datinha)
   return (
     <React.Fragment>
       <ul>
@@ -34,18 +53,11 @@ export default function CardComponent(route) {
         ))}
       </ul>
       <div className="grid my-5">
-        <div className="image grid-form">
-          <img src={luke}/>
-        </div>
-        <div className="image grid-form ml-2">
-          <img src={leia}/>
-        </div>
-        <div className="image grid-form ml-2">
-          <img src={darth}/>
-        </div>
-        <div className="image grid-form ml-2">
-          <img src={han}/>
-        </div>
+        {datinha.map(character => (
+          <div key={Math.random()} className="image grid-form">
+            <img src={character.path}/>
+          </div>
+        ))}
       </div>
     </React.Fragment>
   )
