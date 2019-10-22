107dc6829d878591599a5f288723b751ab9edaa1
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-10-22 15:43:28 -0300

    changes

---
+++ src/Components/Cards/index.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import './style.sass';
+import luke from '../../assets/luke.jpg';
+import leia from '../../assets/leia.jpg';
+import darth from '../../assets/darth-vader.jpg';
+import c3po from '../../assets/c3po.jpg';
+import r2d2 from '../../assets/r2-d2.jpg';
+import defaultImage from '../../assets/default.jpg';
+import ky from 'ky';
+
+
+export default function CardsComponent(route) {
+  const getData = () => ky.get(`https://swapi.co/api/${route.name}`).json();
+  const [apiReturn, setReturn] = useState([]);
+  const [objects, setObjects] = useState([]);
+  const [actualRoute, setRoute] = useState('');
+  const [imagesCharacters] = useState([
+    {
+      people: [
+        { name: 'luke', path: luke },
+        { name: 'c', path: c3po },
+        { name: 'r2', path: r2d2 },
+        { name: 'darth', path: darth },
+        { name: 'leia', path: leia},
+        { name: 'owen', path: defaultImage},
+        { name: 'beru', path: defaultImage},
+        { name: 'r5', path: defaultImage},
+        { name: 'biggs', path: defaultImage},
+        { name: 'obi', path: defaultImage}
+      ],
+      planets: [
+        { name: 'alderaan', path: r2d2 }
+      ]
+    }
+  ]);
+  const data = apiReturn.map(item => item.results)
+
+  async function loadReturn() {
+    const response = await getData();
+    setReturn([response]);
+  }
+
+  function getImages(actualRoute) {
+    const object = [];
+    data.forEach(element => {
+      element.forEach(element => {
+        imagesCharacters.forEach(item => {
+          item[actualRoute.name].forEach(data => {
+            if (data.name === element.name.split((/[ -]/))[0].toLowerCase()) {
+              object.push({ object: element, path: data.path })
+            }
+          })
+        })
+      })
+    })
+    setObjects(object)
+  }
+
+  useEffect(() => {
+    setRoute(route);
+    loadReturn();
+  }, []);
+
+  useEffect(() => {
+    getImages(actualRoute);
+  }, [apiReturn]);
+
+  return (
+    <React.Fragment>
+      <div className="grid my-5">
+        <Row>
+          {objects.map((data, index) => (
+            ['top'].map(placement => (
+              <OverlayTrigger
+                key={placement}
+                placement={placement}
+                overlay={
+                  <Tooltip id={`tooltip-${placement}`}>
+                    <p className="font-sw">{data.object.name}</p>
+                  </Tooltip>
+                }
+              >
+                <Col md={3}>
+                  <Link to={actualRoute.name + `/` + (index + 1) }>
+                    <div key={index} className="image grid-form">
+                      <img src={data.path} />
+                    </div>
+                  </Link>
+                </Col>
+              </OverlayTrigger>
+            ))
+          ))}
+        </Row>
+      </div>
+    </React.Fragment>
+  )
+}
--- src/Components/GridComponent/index.js
+++ src/Components/GridComponent/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import CardComponent from '../Card/index.js';
+import CardsComponent from '../Cards/index.js';
 
 export default function GridComponent(route) {
   const [routes] = useState([
@@ -11,7 +11,7 @@ export default function GridComponent(route) {
   const actualRoute = routes.find(item => item.route === route.match.path.split('/')[1])
   return(
     <React.Fragment>
-      <CardComponent name={actualRoute.newRoute}/>
+      <CardsComponent name={actualRoute.newRoute}/>
     </React.Fragment>
   )
 }
