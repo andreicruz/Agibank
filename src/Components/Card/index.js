ce4f3f7b84a3ba22f3c0fcb0f40f6d32aea74db2
andreicruz <andrei_cruz97@hotmail.com>
2019-10-22 23:27:58 -0300

    component return added

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,1 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import classNames from "classnames";
+import './style.sass';
+
+export default function Card(element){
+  const [isHovered, setHovered] = useState(false);
+  return(
+    <React.Fragment>
+      <div className="grid-form grid-form--card">
+        <div className={classNames("background", {"flip-card": isHovered})}
+            style={{backgroundImage: `url(${element.path})`}}
+            onMouseEnter={() => setHovered(true)} 
+            onMouseOut={() => setHovered(!isHovered)}
+        >
+              
+          
+        </div>
+      </div>
+      {/* <div key={element.index} className="image grid-form">
+        <img 
+          src={element.path}
+          className={classNames({"flip-card": isHovered})}
+          onMouseEnter={() => setHovered(true)} 
+          onMouseOut={() => setHovered(!isHovered)}
+        />
+        {isHovered ? '😁' : '☹️'}
+      </div> */}
+    </React.Fragment>
+  )
+}
