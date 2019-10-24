6dfd49301035aec364f239c9408dd113121d929c
andreicruz <andrei_cruz97@hotmail.com>
2019-10-23 23:36:29 -0300

    changes

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,34 +3,33 @@ import classNames from "classnames";
 import './style.sass';
 
 export default function Card(element){
-  console.log(`index `, element.index)
-  console.log(`path `, element.path)
-  console.log(`object `, element.data.object)
+  // console.log(`index `, element.index)
+  // console.log(`path `, element.path)
+  // console.log(`object `, element.data.object.name)
   const [isHovered, setHovered] = useState(false);
   const [isClicked, setClick] = useState(false);
+
+  function mouseOut(){
+    if(isClicked){
+      setHovered(true)
+    }else{
+      setHovered(!isHovered)
+    }
+  }
 
   return(
     <React.Fragment>
       {/* <div className="grid-form grid-form--card"> */}
         <div onClick={() => setClick(!isClicked)}
-            className={classNames("background", {"hover-card": isHovered}, {"flip-card": isClicked})}
+            className={classNames("background", {"hover-card": isHovered}, {"flip-card": isClicked}, {"active-card": isClicked})}
             style={{backgroundImage: `url(${element.path})`}}
             onMouseEnter={() => setHovered(true)} 
-            onMouseOut={() => setHovered(!isHovered)}
+            onMouseOut={() => mouseOut()}
         >
-              
+              <p>{element.data.object.name}</p>
           
         </div>
       {/* </div> */}
-      {/* <div key={element.index} className="image grid-form">
-        <img 
-          src={element.path}
-          className={classNames({"flip-card": isHovered})}
-          onMouseEnter={() => setHovered(true)} 
-          onMouseOut={() => setHovered(!isHovered)}
-        />
-        {isHovered ? '😁' : '☹️'}
-      </div> */}
     </React.Fragment>
   )
 }
