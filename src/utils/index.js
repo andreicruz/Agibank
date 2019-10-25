9d48f16884a39e00d5f13aa2d6a3b4cb7bebcb39
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-10-25 18:51:43 -0300

    new images

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,10 @@
-import hope from '../assets/hope.jpg';
-import returnJedi from '../assets/return.jpg';
-import revenge from '../assets/revenge.jpg';
-import empire from '../assets/empire.jpg';
-
+import hope from '../assets/hope1.jpg';
+import returnJedi from '../assets/return1.jpg';
+import revenge from '../assets/revenge1.jpg';
+import empire from '../assets/empire1.jpg';
+import attack from '../assets/attack1.jpg';
+import force from '../assets/force.jpg';
+import phantom from '../assets/phantom1.jpg';
 
 export const listObjects = () => {
   const allMovies = [{
@@ -10,7 +12,10 @@ export const listObjects = () => {
         { name: 'hope', path: hope },
         { name: 'return', path: returnJedi },
         { name: 'empire', path: empire },
-        { name: 'revenge', path: revenge }
+        { name: 'revenge', path: revenge },
+        { name: 'attack', path: attack },
+        { name: 'force', path: force },
+        { name: 'phantom', path: phantom },
       ],
       people: [
         { name: 'hope', path: hope }
