255b4ea4e0cdb390987bbe88cd89ed4f3b3d43fd
andreicruz <andrei_cruz97@hotmail.com>
2019-10-28 23:18:11 -0300

    changes in mobile

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Container from 'react-bootstrap/Container';
 import './style.sass';
 import { Link } from "react-router-dom";
 import header from '../../assets/header.png';
+import { faBars, faHome } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
-  const [top] = useState(false);
-  document.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-      console.log('scrollei')
-    }
-  })
-
+  const [showMenu, setDisplayMenu] = useState(false);
   const [routes] = useState([
     { path: 'people'},
     { path: 'films' },
@@ -30,14 +26,34 @@ export default function Navbar() {
                   <div className="background">                                      
                     <Link to={`/` + data.path}>{data.path}</Link>
                   </div>
-                  {/* <a className="font-sw" href="#people">
-                    {data.path}
-                  </a> */}
                 </div>
               ))}
             </nav>
             </Container>
           </div>
+      </header>
+      <header className="header-mobile d-xs-block d-md-none">
+        <Container>
+          <div className="itens">
+            <div className="home">
+              <Link to="/home"><FontAwesomeIcon size={'2x'} icon={faHome}/></Link>
+            </div>
+            <div className="menu">
+              <span onClick={() => setDisplayMenu(!showMenu)}>
+                <FontAwesomeIcon size={'2x'} icon={faBars} />
+              </span>
+            </div>
+          </div>
+          <div style={{display: showMenu === true ? 'block' : 'none'}} className="hidden-menu">
+            <ul className="nav navbar-nav">
+              {routes.map((data, index) => (
+                <li key={index}>
+                  <Link onClick={() => setDisplayMenu(false)} to={`/` + data.path}>{data.path}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Container>
       </header>
     </React.Fragment>
   )
