08aa0509756f8cc3a096f3daca1a86cad67f1ff8
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-10-31 15:55:03 -0300

    test file

--- src/Components/Banner/banner.test.js
+++ src/Components/Banner/banner.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Banner from './index.js';
 import { mount } from 'enzyme';
 import toJson from "enzyme-to-json"
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-configure({ adapter: new Adapter() });
+// import { configure } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-15';
+// configure({ adapter: new Adapter() });
+
 it("should display a banner with links", () => {
   const props = {
     title: 'people',
