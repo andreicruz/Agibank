15ad7a1c5b40e1021bc1a8d5dfadb47f27bf69c9
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-10-31 15:45:54 -0300

    unit test

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './style.sass';
 import { Link } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 export default function Banner({title, image}) {
   return(
-    <Link to={title}>
-      <div className="banner">
-        <div className="background" style={{backgroundImage : `url(${image})`}}  >
-          <h3>{title}</h3>
+    <Router>
+      <Link to={title}>
+        <div className="banner">
+          <div className="background" style={{backgroundImage : `url(${image})`}}  >
+            <h3>{title}</h3>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </Router>
     )
   }
